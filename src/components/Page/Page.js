9c68f712e897a452074bfc4b0918c68605994ef5
maxwellwachira <maxwellwachira67@gmail.com>
2022-04-16 02:32:17 +0300

    addition of comments

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -5,11 +5,15 @@ import { usePageFetch } from '../../hooks/usePageFetch';
 import { Wrapper, TextButton, Content, ButtonWrapper, StyledModal } from './Page.styles';
 
 const Page = () => {
+    //hooks from usePageFetch
     const {pages, loading, pageNumber, text, isOpen, textClick, prevClick, nextClick, toggleModal} = usePageFetch();
+    //array with the indices of the left and right pages
     var indices = [pageNumber * 2 - 2, pageNumber * 2 - 1];
+    //array to hold content and tokenized data of the left and right pages
     var results = [];
     indices.forEach(i => results.push(pages[i]));
 
+    //Return Loading while data is being fetched from GraphQL endpoint
     if(loading){
         return(
           <div>
@@ -17,15 +21,17 @@ const Page = () => {
           </div>
         )
     }
-  
+    //Return this once loading is done
     return (
       <>
         <Wrapper>
             <Content>
               {results.map((result, index) => {
+                //make content an array 
                 var contentArray = result.content.split(" ");
                 return ( 
                   <p key={index}>{
+                    //map through the content array and make each word clickable
                       contentArray.map((el, index) => (
                           <TextButton key = {index} onClick = {e => textClick(index , contentArray, result)}>{
                           el + " "
@@ -34,12 +40,14 @@ const Page = () => {
                   </p>
                 )} 
               )} 
+              {/* Previous and Next Buttons. Previous button hides when pageNumber = 1 and Next button hides on the last age */}
               <ButtonWrapper>
                   <button onClick={prevClick} className={pageNumber === 1 ? 'hidden' : ''}>Previous</button>
                   <button onClick={nextClick} className={pageNumber === pages.length/2  ? 'hidden' : ''}>next</button>
               </ButtonWrapper>
             </Content>
         </Wrapper>
+       {/* Modal that displays clicked text */}
         <StyledModal
             isOpen={isOpen}
             onBackgroundClick={toggleModal}
